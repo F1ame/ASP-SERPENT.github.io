98624bdb522ab406d090488b5bbe777ba6ff7419
ASP-SERPENT <33945747+ASP-SERPENT@users.noreply.github.com>
2017-11-23 22:12:11 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,25 @@ Home Media About
 */
 var Page = {
 Head: "<!doctype html><html><head><title>ASP_SERPENT Website</title><link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\"><link rel=\"icon\" type=\"image/jpg\" href=\"https://images.mixer.com/64x64/https://uploads.mixer.com/avatar/awvvsuuf-1632056.jpg\"><script src=\"script.js\"></script>",
-NavBar: "<ul class=\"navul\"><li class=\"navli\"><a class=\"nava\" onclick=\"loadPage(\'home\');\">Home</a></li><li class=\"navli\"><a class=\"nava\" onclick=\"loadPage(\'media\')\">Media</a></li><li class=\"navli\"><a class=\"nava\" onclick=\"loadPage(\'about\')\">about</a></li></ul><br>",
-
-
+NavBar: "<ul class=\"navul\"><li class=\"navli\"><a class=\"nava\" onclick=\"loadPage(\'home\');\">Home</a></li><li class=\"navli\"><a class=\"nava\" onclick=\"loadPage(\'media\')\">Media</a></li><li class=\"navli\"><a class=\"nava\" onclick=\"loadPage(\'about\')\">About</a></li></ul><br>",
+Home: "<div class=\"CenterHor CenterVert absolute\"><h1 style=\"color: red;\">CUCK</h1></div>",
+Media: "<
 
 
 
 }
+loadPage = function(pagename) {
+  pageconts = Page.Head + "</head><body>" + Page.NavBar;
+  switch(pagename) {
+    case "home":
+      pageconts = pageconts + Page.Home + "</body></html>";
+    break;
+    case "media":
+      pageconts = pageconts + Page.Media + "</body></html>;
+      break;
+    case "about":
+      pageconts = pageconts + Page.About + "</body></html>";
+      break;
+  }
+  document.write(pageconts);
+};
