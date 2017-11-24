276ee95eaecb924aed8d91fe8899b9308dae3cee
ASP-SERPENT <33945747+ASP-SERPENT@users.noreply.github.com>
2017-11-24 00:21:55 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ Home Media About
 var Page = {
 Head: "<head><title>ASP_SERPENT Website</title><link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\"><link rel=\"icon\" type=\"image/jpg\" href=\"https://images.mixer.com/64x64/https://uploads.mixer.com/avatar/awvvsuuf-1632056.jpg\"><script src=\"script.js\"></script>",
 Chat: "<iframe class=\"sidebar\" allowfullscreen=\"true\" src=\"https://mixer.com/embed/chat/1114958\"></iframe>",
-  NavBar: "<ul class=\"navul\"><li class=\"navli\"><a class=\"nava\" onclick=\"loadPage(\'home\');\">Home</a></li><li class=\"navli\"><a class=\"nava\" onclick=\"loadPage(\'media\')\">Media</a></li><li class=\"navli\"><a class=\"nava\" onclick=\"loadPage(\'about\')\">Stream</a></li></ul><br>",
+  NavBar: "<ul class=\"navul\"><li class=\"navli\"><a class=\"nava\" onclick=\"loadPage(\'home\');\">Home</a></li><li class=\"navli\"><a class=\"nava\" onclick=\"loadPage(\'media\')\">Watch</a></li><li class=\"navli\"><a class=\"nava\" onclick=\"loadPage(\'about\')\">About</a></li><li class=\'navli\'><a class=\'nava\' href=\'https://mixer.com/ASP_SERPENT\'>Go To Stream</a></li></ul><br>",
 Home: "<div class=\"CenterHor CenterVert absolute\"><h1 style=\"color: red;\">CUCK</h1></div>",
 Media: "<iframe class=\"vide\" allowfullscreen=\"true\" src=\"https://mixer.com/embed/player/1114958\"></iframe><br><iframe src=\"https://discordapp.com/widget?id=310954900823474177&theme=dark\" class=\"disc\" allowtransparency=\"true\" frameborder=\"0\"></iframe>",
-About: "<h1><a href=\"https://discord.gg/Sr3bT8J\">ASP_SERPENT Discord Server</a></h1><div>Site put together by clockdaddy</div>"
+About: "<h1>Rule #1 <br>No Spoilers<br>Rule #2<br> There are no other rules</h1><div>Site put together by clockdaddy</div>"
 }
 loadPage = function(pagename) {
   pageconts = Page.Head + Page.NavBar + Page.Chat + "</head><body>";
