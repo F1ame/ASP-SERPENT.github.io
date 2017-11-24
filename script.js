991fd5037b6267e12655a9122c1ebdb6033344aa
ASP-SERPENT <33945747+ASP-SERPENT@users.noreply.github.com>
2017-11-23 21:45:36 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+/*asp wants this stuff
+homepage giant text saying -CUCK-
+
+
+pages
+Home Media About
+*/
+var Page = {
+Head: "<!doctype html><html><head><title>ASP_SERPENT Website</title><link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\"><link rel=\"icon\" type=\"image/jpg\" href=\"https://images.mixer.com/64x64/https://uploads.mixer.com/avatar/awvvsuuf-1632056.jpg\"><script src=\"script.js\"></script>",
+NavBar: "<ul class=\"navul\"><li class=\"navli\"><a class=\"nava\" onclick=\"loadPage(\'home\');\">Home</a></li><li class=\"navli\"><a class=\"nava\" onclick=\"loadPage(\'media\')\">Media</a></li><li class=\"navli\"><a class=\"nava\" onclick=\"loadPage(\'about\')\">about</a></li></ul><br>",
+
+
+
+
+
+}
