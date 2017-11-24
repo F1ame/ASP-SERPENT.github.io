3af96763b225641f34e4ad2f39e82bb8e73480c1
ASP-SERPENT <33945747+ASP-SERPENT@users.noreply.github.com>
2017-11-23 22:57:35 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ pages
 Home Media About
 */
 var Page = {
-Head: "<!doctype html><html><head><title>ASP_SERPENT Website</title><link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\"><link rel=\"icon\" type=\"image/jpg\" href=\"https://images.mixer.com/64x64/https://uploads.mixer.com/avatar/awvvsuuf-1632056.jpg\"><script src=\"script.js\"></script>",
+Head: "<head><title>ASP_SERPENT Website</title><link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\"><link rel=\"icon\" type=\"image/jpg\" href=\"https://images.mixer.com/64x64/https://uploads.mixer.com/avatar/awvvsuuf-1632056.jpg\"><script src=\"script.js\"></script>",
 NavBar: "<ul class=\"navul\"><li class=\"navli\"><a class=\"nava\" onclick=\"loadPage(\'home\');\">Home</a></li><li class=\"navli\"><a class=\"nava\" onclick=\"loadPage(\'media\')\">Media</a></li><li class=\"navli\"><a class=\"nava\" onclick=\"loadPage(\'about\')\">About</a></li></ul><br>",
 Home: "<div class=\"CenterHor CenterVert absolute\"><h1 style=\"color: red;\">CUCK</h1></div>",
 Media: "<div></div>",
@@ -16,14 +16,14 @@ loadPage = function(pagename) {
   pageconts = Page.Head + Page.NavBar + "</head><body>";
   switch(pagename) {
     case "home":
-      pageconts = pageconts + Page.Home + "</body></html>";
+      pageconts = pageconts + Page.Home + "</body>";
     break;
     case "media":
-      pageconts = pageconts + Page.Media + "</body></html>";
+      pageconts = pageconts + Page.Media + "</body>";
       break;
     case "about":
-      pageconts = pageconts + Page.About + "</body></html>";
+      pageconts = pageconts + Page.About + "</body>";
       break;
   }
-  document.innerHTML = pageconts;
+  document.getElementById("bdy").innerHTML = pageconts;
 };
