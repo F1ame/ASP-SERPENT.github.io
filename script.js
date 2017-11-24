625b5205ecc8e83fa0f03923ebb891370ea6454c
ASP-SERPENT <33945747+ASP-SERPENT@users.noreply.github.com>
2017-11-23 22:40:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ var Page = {
 Head: "<!doctype html><html><head><title>ASP_SERPENT Website</title><link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\"><link rel=\"icon\" type=\"image/jpg\" href=\"https://images.mixer.com/64x64/https://uploads.mixer.com/avatar/awvvsuuf-1632056.jpg\"><script src=\"script.js\"></script>",
 NavBar: "<ul class=\"navul\"><li class=\"navli\"><a class=\"nava\" onclick=\"loadPage(\'home\');\">Home</a></li><li class=\"navli\"><a class=\"nava\" onclick=\"loadPage(\'media\')\">Media</a></li><li class=\"navli\"><a class=\"nava\" onclick=\"loadPage(\'about\')\">About</a></li></ul><br>",
 Home: "<div class=\"CenterHor CenterVert absolute\"><h1 style=\"color: red;\">CUCK</h1></div>",
-Media: "<"
-
+Media: "<div></div>",
+About: "<div>Page put together by clockdaddy</div>"
 
 
 }
@@ -27,5 +27,5 @@ loadPage = function(pagename) {
       pageconts = pageconts + Page.About + "</body></html>";
       break;
   }
-  document.innerhtml(pageconts);
+  document.write(pageconts);
 };
