a22efeceaf305acb9f2f514009ef15c9038c9475
ASP-SERPENT <33945747+ASP-SERPENT@users.noreply.github.com>
2017-11-23 23:56:56 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ Home Media About
 */
 var Page = {
 Head: "<head><title>ASP_SERPENT Website</title><link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\"><link rel=\"icon\" type=\"image/jpg\" href=\"https://images.mixer.com/64x64/https://uploads.mixer.com/avatar/awvvsuuf-1632056.jpg\"><script src=\"script.js\"></script>",
-Chat: "<div class=\"sidebar\"><iframe allowfullscreen=\"true\" src=\"https://mixer.com/embed/chat/1114958\"></iframe></div>",
+Chat: "<iframe class=\"sidebar\" allowfullscreen=\"true\" src=\"https://mixer.com/embed/chat/1114958\"></iframe>",
   NavBar: "<ul class=\"navul\"><li class=\"navli\"><a class=\"nava\" onclick=\"loadPage(\'home\');\">Home</a></li><li class=\"navli\"><a class=\"nava\" onclick=\"loadPage(\'media\')\">Media</a></li><li class=\"navli\"><a class=\"nava\" onclick=\"loadPage(\'about\')\">About</a></li></ul><br>",
 Home: "<div class=\"CenterHor CenterVert absolute\"><h1 style=\"color: red;\">CUCK</h1></div>",
 Media: "<iframe class=\"vide\" allowfullscreen=\"true\" src=\"https://mixer.com/embed/player/1114958\"></iframe><br><iframe src=\"https://discordapp.com/widget?id=310954900823474177&theme=dark\" width=\"350\" height=\"500\" allowtransparency=\"true\" frameborder=\"0\"></iframe>",
